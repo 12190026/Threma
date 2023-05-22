b394e9edce79dca9301befd5bd3a97607137d204
12190026 <12190026.gcit@rub.edu.bt>
2023-05-22 11:30:24 +0600

    up

--- trama/settings.py
+++ trama/settings.py
@@ -172,12 +172,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = ''
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Media files configuration
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
